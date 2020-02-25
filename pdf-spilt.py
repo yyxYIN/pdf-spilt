ac897795f99ee660fafcd1298cea0808cf7a4f53
yyx <420358742@qq.com>
2020-02-25 20:25:19 +0800

    '修复了一个会导致最后一段内容出错的bug'

--- pdf-spilt.py
+++ pdf-spilt.py
@@ -1,4 +1,5 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
+import math
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -17,7 +18,7 @@ def spilt_pdf(read_file, output_path, out_page_nums):
         page_count = pdf_input_file.getNumPages()
         logging.info('page count is %d', page_count)
 
-        loop_num = round(page_count / out_page_nums)
+        loop_num = math.floor(page_count / out_page_nums)
         logging.info('loop count is %d', loop_num)
         last_pages_num = page_count - out_page_nums * loop_num
         logging.info('last_pages_num count is %d', last_pages_num)
@@ -42,7 +43,7 @@ def spilt_pdf(read_file, output_path, out_page_nums):
                     pdf_output_file.addPage(pdf_input_file.getPage(i))
                 with open(pdf_output_name, 'wb') as sub_fp:
                     pdf_output_file.write(sub_fp)
-                logging.info('完成分割%d到%d页', i + 1, i + out_page_nums)
+                logging.info('完成分割%d到%d页', out_page_nums * loop_num + 1, out_page_nums * loop_num + last_pages_num)
             except IndexError as e:
                 logging.debug(e)
 
@@ -54,6 +55,6 @@ def spilt_pdf(read_file, output_path, out_page_nums):
 
 
 if __name__ == "__main__":
-    FILE_NAME = "C:\\Users\\42035\Desktop\\linearalgebra\\Introduction to Linear Algebra(5th edition 2016).pdf"
-    output_path = "C:\\Users\\42035\Desktop\\linearalgebra"
-    spilt_pdf(FILE_NAME, output_path, 80)
+    FILE_NAME = "C:\\Users\\42035\Desktop\\gooddellow\\Goodfellow - 2016 - NIPS 2016 Tutorial Generative Adversarial Networks.pdf"
+    output_path = "C:\\Users\\42035\Desktop\\gooddellow"
+    spilt_pdf(FILE_NAME, output_path, 20)
